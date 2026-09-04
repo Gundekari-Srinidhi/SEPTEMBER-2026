102fff407dfe5f6a02f6a364f8515068933cf5af
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2026-09-04 15:21:50 +0530

    Time: 557 ms (57.26%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -3,4 +3,10 @@
 #SELECT project_id,ROUND(AVG(experience_years),2) as average_years FROM Project as p left JOIN Employee as e ON p.employee_id=e.employee_id GROUP BY p.project_id;
 
 
-select project_id,round(avg(experience_years),2) as average_years FROM Project as p left JOIN Employee as e ON p.employee_id=e.employee_id group by project_id;
+#select project_id,round(avg(experience_years),2) as average_years FROM Project as p left JOIN Employee as e ON p.employee_id=e.employee_id group by project_id;
+
+
+
+
+
+select project_id,round(avg(experience_years),2) as average_years from project p join employee e on p.employee_id = e.employee_id group by project_id;
